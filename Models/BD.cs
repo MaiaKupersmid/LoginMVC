d836fae8d8904fe43d0957064a35729add9e031e
JULIETA IZBICKI <47350196@ort>
2023-10-04 08:52:08 -0300

    se me bloqueo

--- Models/BD.cs
+++ Models/BD.cs
@@ -15,12 +15,5 @@ public static class BD
         }
     }
 
-    public static void AgregarUsuario(Usuario user)
-    {
-        string sql = "INSERT INTO Usuario(UserName, Contraseña, Nombre, Mail, Telefono) VALUES(@UserName, @Contraseña, @Nombre, @Mail, @Telefono)";
-        using (SqlConnection BD = new SqlConnection(_connectionString))
-        {
-            BD.Execute(sql, new {UserName = user.UserName, Contraseña = user.Contraseña, Nombre = user.Nombre, Mail = user.Mail, Telefono = user.Telefono });
-        }
-    }
+    
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,34 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//document.addEventListener("DOMContentLoaded", () => {
+
+/*opcion 1: usando onsubmit
+function checkForm()
+{
+    alert("ceckform");
+    const contraseña1 = document.getElementById("Contraseña1");
+
+    const especiales = ["#", "$", "%", "&", "/", "@"];
+    Contraseña1.includes(especiales);
+    
+    const contraseña2 = document.getElementById("Contraseña2");
+
+}
+
+*/
+
+
+//opcion 2: escuchar cuando escribe algo
+
+document.getElementsById("Contrasena1").addEventListener('keypress', () =>
+{
+    const contrasena1 = document.getElementById("Contrasena1");
+
+    const especiales = ["#", "$", "%", "&", "/", "@"];
+    Contrasena1.includes(especiales);
+    
+    const contrasena2 = document.getElementById("Contrasena2");
+}
+);
